9ffbd5572beb46c99cfacbdcca5fc689d4d390f3
guokeke <guo.ke.ke.a@gmail.com>
2017-03-11 01:32:48 +0800

    Change err to constants

--- src/core/api.js
+++ src/core/api.js
@@ -3,7 +3,6 @@ import fetch from './fetch';
 import history from './history';
 import appState from './state';
 
-
 const defaultOptions = {
   method: 'post',
   headers: {
@@ -12,40 +11,51 @@ const defaultOptions = {
   },
 };
 
-function api(url, options = {}, checkToken = true) {
-  const authToken = Lockr.get('token');
-  if (checkToken && !authToken) {
-    history.push('/login');
-    appState.update('login', false);
-    throw new Error(`no token ${url}`);
-  }
-  const headers = Object.assign({}, defaultOptions.headers, options.headers, {
-    Authorization: `Bearer ${authToken}`,
-  });
+export const ERR_400 = new Error('400');
+export const ERR_401 = new Error('401');
+export const ERR_404 = new Error('404');
+export const ERR_500 = new Error('500');
+export const ERR_NO_TOKEN = new Error('no token!');
 
-  return fetch(url, Object.assign({}, defaultOptions, options, {
-    headers,
-  }))
-  .then((res) => {
-    switch (res.status) {
-      case 400:
-        // history.push('/login');
-        throw new Error('400');
-      case 401:
+function api(url, options = {}, checkToken = true) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const authToken = Lockr.get('token');
+      if (checkToken && !authToken) {
         history.push('/login');
         appState.update('login', false);
-        throw new Error('401');
-      case 404:
-        // history.push('/404');
-        throw new Error('404');
-      case 500:
-        history.push('/500');
-        throw new Error('500');
-      default:
-        break;
-    }
+        throw ERR_NO_TOKEN;
+      }
 
-    return res;
+      const headers = Object.assign({}, defaultOptions.headers, options.headers, {
+        Authorization: `Bearer ${authToken}`,
+      });
+      const res = await fetch(url, Object.assign({}, defaultOptions, options, {
+        headers,
+      }));
+
+      switch (res.status) {
+        case 201:
+        case 200:
+          resolve(res);
+          break;
+        case 400:
+          throw ERR_400;
+        case 401:
+          history.push('/login');
+          appState.update('login', false);
+          throw ERR_401;
+        case 404:
+          throw ERR_404;
+        case 500:
+          history.push('/500');
+          throw ERR_500;
+        default:
+          break;
+      }
+    } catch (err) {
+      reject(err);
+    }
   });
 }
 
