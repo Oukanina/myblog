f4e7cb6d4d3eda55c005a5a7c38253824ca0b009
guokeke <guo.ke.ke.a@gmail.com>
2017-03-11 01:03:00 +0800

    Fix logout bug

--- src/commands/logout.js
+++ src/commands/logout.js
@@ -3,15 +3,22 @@ import history from '../core/history';
 import appState from '../core/state';
 
 export default {
+
+  name: 'logout',
+
   test: /^\s*logout\s*$/,
 
   action() {
     return new Promise((resolve) => {
-      Lockr.set('token', '');
-      appState.update('login', false);
+      Lockr.set('token', null);
       history.push('/login');
+      const container = appState.get('containerElement');
+      container.style.top = 0;
+      appState.update('login', false);
+      appState.trigger('wheel');
+
       // if need goAhead then pass true
-      resolve(false);
+      resolve(true);
     });
   },
 };
